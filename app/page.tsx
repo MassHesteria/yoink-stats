153ffe8970f42dfcdb6b221d4d44a2ec3d1fdb0a
MassHesteria <brad.hester@gmail.com>
2024-03-29 13:27:35 -0500

    More metadata

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,17 @@ import { fetchMetadata } from "frames.js/next";
 import { getHostName } from "./data";
  
 export async function generateMetadata() {
+  const metaData = await fetchMetadata(new URL("/frames", getHostName()));
   return {
     title: "Yoink Stats",
+    description: "Check your stats on Yoink",
+    metadataBase: new URL(getHostName()),
     // provide a full URL to your /frames endpoint
-    other: await fetchMetadata(
-      new URL("/frames", getHostName())
-    ),
+    other: metaData,
+    openGraph: {
+      title: "Yoink Stats",
+      images: [metaData["fc:frame:image"]],
+    }
   };
 }
  
