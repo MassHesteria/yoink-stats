86351023eb71ff07f124a454804634aafb34c4eb
MassHesteria <brad.hester@gmail.com>
2024-03-29 06:17:43 -0500

    Base functionality

--- app/frames/route.tsx
+++ app/frames/route.tsx
@@ -35,6 +35,33 @@ const getUsers = (stats: Stats) => {
   })
 }
 
+function formatTime(seconds: number) {
+  const days = Math.floor(seconds / (3600 * 24));
+  const hours = Math.floor((seconds % (3600 * 24)) / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const remainingSeconds = seconds % 60;
+
+  const parts = [];
+
+  if (days > 0) {
+    parts.push(`${days}d`);
+  }
+
+  if (hours > 0) {
+    parts.push(`${hours}h`);
+  }
+
+  if (minutes > 0) {
+    parts.push(`${minutes}m`);
+  }
+
+  if (remainingSeconds > 0) {
+    parts.push(`${remainingSeconds}s`);
+  }
+
+  return parts.join(' ');
+}
+
 const IntroPage = () => {
   return (
     <div tw="flex">
@@ -43,25 +70,49 @@ const IntroPage = () => {
           <span tw="font-bold pb-5" style={{color: "#b16286"}}>Do you Yoink? 🚩</span>
           <span tw="font-bold" style={{color: "#8ec07c"}}>Check your stats!</span>
         </h2>
-        {/*<div tw="mt-8 flex md:mt-0">
-          <div tw="flex rounded-md shadow">
-            <a
-              href="#"
-              tw="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base font-medium text-white"
-            >
-              Get started
-            </a>
-          </div>
-          <div tw="ml-3 flex rounded-md shadow">
-            <a
-              href="#"
-              tw="flex items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium text-indigo-600"
-            >
-              Learn more
-            </a>
-          </div>
-        </div>*/}
       </div>
+    </div>
+  )
+}
+
+const RankPage = ({ username, rank, total, yoinks, time }: { username: string, rank: number, total: number, yoinks: number, time: number}) => {
+  const nameSizeClass = username.length > 20 ? "text-7xl" : "text-8xl";
+  return (
+    <div tw="flex h-full">
+      <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
+        <h2 tw="flex flex-col font-bold tracking-tight text-left">
+          <div tw="flex flex-col">
+            <div tw="flex flex-col justify-end">
+              <div tw={`flex ${nameSizeClass}`}>
+                <span tw="" style={{color: "#b16286"}}>{username}</span>
+              </div>
+            </div>
+            <div tw="flex flex-col text-right justify-end">
+              <div tw="flex text-7xl mb-1">
+                {/*<span tw="pr-4" style={{color: "#928374" }}>Rank</span>*/}
+                <span tw="pr-4" style={{color: "#fabd2f"}}>{rank}</span>
+                <span tw="pr-4" style={{color: "#928374"}}>/</span>
+                <span tw="" style={{color: "#fabd2f"}}>{total}</span>
+              </div>
+            </div>
+          </div>
+          <div tw="flex pt-9 flex-wrap">
+            <div tw="flex pr-10">
+              <span tw="" style={{color: "#8ec07c"}}>Yoinks</span>
+              <span tw="pl-4" style={{color: "#458588"}}>{yoinks}</span>
+            </div>
+            <div tw="flex">
+              <span tw="" style={{color: "#8ec07c"}}>Time Held</span>
+              <span tw="pl-4" style={{color: "#458588"}}>{formatTime(time)}</span>
+            </div>
+          </div>
+          <div tw="flex">
+            <span tw="" style={{color: "#8ec07c"}}>Average Time Held</span>
+            <span tw="pl-4" style={{color: "#458588"}}>{formatTime(Math.floor(time/yoinks))}</span>
+          </div>
+        </h2>
+      </div>
+      <span tw="absolute bottom-2 right-4">/yoink 🚩</span>
     </div>
   )
 }
@@ -70,19 +121,33 @@ const handleRequest = frames(async (ctx) => {
   const username = ctx.message?.requesterUserData?.username;
 
   let temp = "";
+  let rank = 10000;
+  let total = 10000;
+  let yoinks = 12345678
+  let time = 1234567890;
   if (username) {
     let res = await fetch("https://yoink.terminally.online/api/stats");
     const stats = await res.json();
     const leaderboard = getUsers(stats).sort((a, b) => b.times - a.times);
-    const rank = leaderboard.findIndex(p => p.username === username)+1;
-    temp = `${rank} of ${leaderboard.length}`
+    rank = leaderboard.findIndex(p => p.username === username)+1;
+    if (rank > 0) {
+      total = leaderboard.length;
+      const user = leaderboard[rank-1];
+      yoinks = user.yoinks;
+      time = user.times;
+      temp = username;
+    }
   }
 
   return {
     image: (
       <div tw="w-full h-full text-white justify-center items-center flex flex-col"
            style={{backgroundColor: "#282828" }}>
+        {username ?
+        <RankPage username={temp} rank={rank} total={total} yoinks={yoinks} time={time} />
+        :
         <IntroPage />
+        }
       </div>
     ),
     buttons: [
