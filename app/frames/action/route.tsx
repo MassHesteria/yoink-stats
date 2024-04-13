6af039b3a1f20e9ef26e5ef8768333ac22cbdb1b
MassHesteria <brad.hester@gmail.com>
2024-04-13 09:38:52 -0500

    Slight tweak

--- app/frames/action/route.tsx
+++ app/frames/action/route.tsx
@@ -104,9 +104,9 @@ export async function POST(req: NextRequest) {
       const rank = leaderboard.findIndex(p => p.userId == `farcaster:${fid}`)+1;
       if (rank > 0) {
         const user = leaderboard[rank-1];
-        message = `# ${rank} ⧖ ${formatTime(user.times)} ⚐ ${user.yoinks}`
+        message = `#${rank} ⧖ ${formatTime(user.times)} ⚐ ${user.yoinks}`
         if (message.length > 30) {
-          message = `# ${rank} ⚐ ${user.yoinks}`
+          message = `#${rank} ⚐ ${user.yoinks}`
         }
         //total = leaderboard.length;
         //yoinks = user.yoinks;
