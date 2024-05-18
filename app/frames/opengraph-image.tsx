7427cdeb6c10729858e11e1bc9531d79f883cc32
MassHesteria <brad.hester@gmail.com>
2024-05-18 10:23:29 -0500

    OG fix

--- app/frames/opengraph-image.tsx
+++ app/frames/opengraph-image.tsx
@@ -1,8 +1,8 @@
 import { ImageResponse } from "next/og";
-import { generateImage } from "./generate";
+import { IntroPage } from "./components/intro";
 
 export default async function Image() {
-  return new ImageResponse(await generateImage(), {
+  return new ImageResponse(<IntroPage />, {
      width: 800,
      height: 420,
   });
